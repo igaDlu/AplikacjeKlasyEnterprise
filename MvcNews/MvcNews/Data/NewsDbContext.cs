6da9943c0468ee9c83cbcae004c0baa5c16e4167
igaDlu <130709902+igaDlu@users.noreply.github.com>
2026-05-31 12:05:53 +0200

    Poprawka gitignore i wyczyszczenie cache

---
+++ MvcNews/MvcNews/Data/NewsDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+using MvcNews.Models;
+
+namespace MvcNews.Data
+{
+    public class NewsDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public NewsDbContext(DbContextOptions<NewsDbContext> options) :
+        base(options)
+        { }
+        public Microsoft.EntityFrameworkCore.DbSet<NewsItem> News { get; set; } = null!;
+    }
+
+}
---
+++ MvcNews/MvcNews/Models/NewsItem.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace MvcNews.Models
+{
+    public class NewsItem
+    {
+        public int Id { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime TimeStamp { get; set; }
+        [Required]
+        [StringLength(140, MinimumLength = 5)]
+        public string Text { get; set; } = string.Empty;
+        [Timestamp]
+        public byte[]? RowVersion { get; set; }
+    }
+}
